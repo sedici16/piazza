d809c3a2f894ee74c0dd6cbb54de6430f2713c7d
sedici16 <monti.g@gmail.com>
2023-12-07 14:51:53 +0100

    Update expirePostsCron.js

--- tasks/expirePostsCron.js
+++ tasks/expirePostsCron.js
@@ -1,7 +1,8 @@
 // expirePostsCron.js
-
+// this cron job will expire posts every five minutes
+//if the time is over the script will update the status of the posts
 const cron = require('node-cron');
-const Post = require('../models/model_of_db'); // Update with the correct path to your Post model
+const Post = require('../models/model_of_db'); 
 
 const expirePostsTask = () => {
   cron.schedule('* * * * *', async () => {
